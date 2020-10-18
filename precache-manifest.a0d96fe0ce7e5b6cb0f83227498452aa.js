75bf6e15fc149fb1a48d4f2dbc802907a6ae441d
unknown <jianxie6464@gmail.com>
2020-10-18 12:25:15 -0700

    Updates

---
+++ precache-manifest.a0d96fe0ce7e5b6cb0f83227498452aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "997a729de8196120dee3a7fad05d2091",
+    "url": "/corotrack/index.html"
+  },
+  {
+    "revision": "9859d1596bf8ed3a8a3f",
+    "url": "/corotrack/static/css/main.285dcb4a.chunk.css"
+  },
+  {
+    "revision": "9c81d42ce088220f209b",
+    "url": "/corotrack/static/js/2.f251060f.chunk.js"
+  },
+  {
+    "revision": "639c28c8ef14bcd37d017fe8f6dc04e2",
+    "url": "/corotrack/static/js/2.f251060f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9859d1596bf8ed3a8a3f",
+    "url": "/corotrack/static/js/main.0319c8e0.chunk.js"
+  },
+  {
+    "revision": "3a93916dcc209e1b1bd8",
+    "url": "/corotrack/static/js/runtime-main.2d89708b.js"
+  }
+]);
