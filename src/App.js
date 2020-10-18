a94e3520a47cdec5534f7ccba01c2379e28cbbe1
unknown <jianxie6464@gmail.com>
2020-10-18 11:52:01 -0700

    Completed Coronavirus Application

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Cards, Chart, CountryPicker} from './Components'
+
+import { fetchData } from './api';
+import styles from './App.module.css';
+
+class App extends React.Component {
+    state = {
+        data: {},
+        country: '',
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+
+        this.setState({
+            data: fetchedData
+        });
+    }
+    
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country)
+
+        this.setState({ data: fetchedData, country:country })
+        console.log(country);
+
+    }
+    render() {
+        const { data, country } = this.state
+        return(
+            <div className={styles.container}>
+                <Cards data={data}/>
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
+                <Chart data={data} country={country}/>
+            </div>
+        )
+    }
+}
+
+export default App;
